bb23030fd96682eef0f689e2c3b1c5824cfe00ce
prince3017 <55761062+prince3017@users.noreply.github.com>
2024-03-11 12:27:54 +0530

    Add files via upload

---
+++ Script.sql
@@ -0,0 +1,51 @@
+create database IMS
+
+USE ims
+
+CREATE TABLE product_category (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    `desc` TEXT, 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP NULL
+);
+
+
+CREATE TABLE product_inventory (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    quantity INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP NULL
+);
+
+CREATE TABLE discount (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    `desc` TEXT,
+    discount_percent DECIMAL(5, 2) NOT NULL,
+    active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP NULL
+);
+
+
+
+CREATE TABLE product (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    `desc` TEXT,
+    SKU VARCHAR(255) NOT NULL,
+    category_id INT NOT NULL,
+    inventory_id INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    discount_id INT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP NULL,
+    FOREIGN KEY (category_id) REFERENCES product_category(id),
+    FOREIGN KEY (inventory_id) REFERENCES product_inventory(id),
+    FOREIGN KEY (discount_id) REFERENCES discount(id)
+);
